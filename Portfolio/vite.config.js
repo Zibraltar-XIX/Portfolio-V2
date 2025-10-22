13ba592cbdb87d06b1206e5566e19da5e41b9065
Parzival <johann.parola@vulcore.dev>
2025-10-22 19:14:11 +0200

    fix css

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+        allowedHosts: ['unstriking-rosalva-abashedly.ngrok-free.dev'],
+        hmr: {
+            clientPort: 443
+        }
+    }
 })
